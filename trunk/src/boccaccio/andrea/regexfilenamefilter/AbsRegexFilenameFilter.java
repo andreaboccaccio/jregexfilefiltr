17130be28f0f2e185bad7749ca2ce7b6869a4f90
Andrea Boccaccio <andrea@andreaboccaccio.com>
2008-12-24 08:19:00 +0000

    trunk: v.: 0.0.1. Corrected information about license and added a howto file.

--- trunk/src/boccaccio/andrea/regexfilenamefilter/AbsRegexFilenameFilter.java
+++ trunk/src/boccaccio/andrea/regexfilenamefilter/AbsRegexFilenameFilter.java
@@ -4,20 +4,20 @@
  * phone:	+393388098951
  * 
  *
- * This file is part of regexfilenamefilter.
+ * This file is part of JavaRegexFilenameFilter.
  *
- *  regexfilenamefilter is free software: you can redistribute it and/or modify
+ *  JavaRegexFilenameFilter is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  regexfilenamefilter is distributed in the hope that it will be useful,
+ *  JavaRegexFilenameFilter is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with regexfilenamefilter.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with JavaRegexFilenameFilter.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package boccaccio.andrea.regexfilenamefilter;
